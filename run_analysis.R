ab9ab631b71f0d8e7a1e172bfe7a09d99718989e
Marcus E. Turnbo <marcus@emarcus.com>
2014-10-25 00:34:49 -0500

    run_analysis draft 2

--- run_analysis.R
+++ run_analysis.R
@@ -7,22 +7,42 @@ activities <- activities[,2]
 
 columnWidths <- replicate(length(features), 16)
 
+# get test data
 testSubject <- read.table('./data/UCI HAR Dataset/test/subject_test.txt', header=FALSE, col.names=c("subject"))
 testActivity <- read.table('./data/UCI HAR Dataset/test/y_test.txt', header=FALSE, col.names=c("activity"))
 testTimeFrequency <- read.table('./data/UCI HAR Dataset/test/X_test.txt', header=FALSE, col.names=features)
 testData <- cbind(testSubject, type='TEST', testActivity, testTimeFrequency)
 testData$activity <- activities[testData$activity]
 
+# get training data
 trainingSubject <- read.table('./data/UCI HAR Dataset/train/subject_train.txt', header=FALSE, col.names=c("subject"))
 trainingActivity <- read.csv('./data/UCI HAR Dataset/train/y_train.txt', header=FALSE, col.names=c("activity"))
 trainingTimeFrequency <- read.table('./data/UCI HAR Dataset/train/X_train.txt', col.names=features)
 trainingData <- cbind(trainingSubject, type='TRAINING', trainingActivity, trainingTimeFrequency)
 trainingData$activity <- activities[trainingData$activity]
 
+# combine test and training data
 combinedData <- rbind(testData, trainingData)
 
-meanStdColumns <- c("subject","type","activity", grep("std|mean", names(combinedData), value=TRUE))
+# get a subset of mean and standard deviation columns
+meanStdColumns <- c("subject","activity", grep("(mean|std)", names(combinedData), value=TRUE))
 meanStdData <- combinedData[, meanStdColumns]
 
+# Create meaningful column names
+names(meanStdData) <- gsub("^t", "Time", names(meanStdData))
+names(meanStdData) <- gsub("^f", "Frequency", names(meanStdData))
+names(meanStdData) <- gsub("-mean\\(\\)", "Mean", names(meanStdData))
+names(meanStdData) <- gsub("-std\\(\\)", "StdDev", names(meanStdData))
+names(meanStdData) <- gsub("-", "", names(meanStdData))
+names(meanStdData) <- gsub("BodyBody", "Body", names(meanStdData))
 
+# get mean values
+library(reshape2)
+library(data.table)
+melted <- melt(meanStdData, id.vars=c("subject","activity"))
+dt <- data.table(melted)
+agg <- aggregate(value~subject+activity+variable, data=dt, mean)
+
+# write file
+write.table(agg, "./data/tidyMeans.txt", row.names=FALSE)
 
